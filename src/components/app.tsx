bd45c99d3303f273d192edd3645dd2b00b5e9e89
tdillman <tia.dillman@healthcatalyst.com>
2021-01-17 23:05:19 -0700

    Added under construction page

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -9,6 +9,7 @@ import NavBar from './nav-bar/nav-bar';
 import Footer from './footer/footer';
 import Home from './home/home';
 import RacePage from './race-page/race-page';
+import UnderConstruction from './under-construction';
 
 export default function App() {
   return (
@@ -21,6 +22,7 @@ export default function App() {
           <Route path="/home" component={Home} />
           <Route path="/unicorns-unleashed" component={Home} />
           <Route path="/race-page/:id" component={RacePage} />
+          <Route path="*" component={UnderConstruction} />
         </Switch>
 
         <Footer />
---
+++ src/components/under-construction.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UnderConstructionImage from '../images/under-construction.png';
+
+function UnderConstruction() {
+  return (
+    <div className="under-construction">
+      <img src={UnderConstructionImage} alt="under-construction" />
+    </div>
+  );
+}
+
+export default UnderConstruction;
